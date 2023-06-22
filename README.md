d4f4e3f8786bf77b6dffff974e5202bc1a7d87b2
DESKTOP-C8583PR\kwang <kwangho410@gmail.com>
2023-06-22 23:15:36 +0900

    [No4_1] 학급회장 선출

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # infearnAlgorithm
 인프런 강의 : 자바(Java) 알고리즘 문제풀이 입문 - 김태원 
+
+1. sliding window (two pointer)
+- 정의 : two pointer 알고리즘 방식을 활용하며, 배멸내부를 고정 사이즈의 윈도우 (=element 갯수) 가 이동하면서 윈도우내에 있는 데이터를 이용하는 알고리즘
+- 사용예시 : 연속된 N일 동안의 최대 매출액 
--- src/section4_hash_set/No4_1_ClassLeader.java
+++ src/section4_hash_set/No4_1_ClassLeader.java
@@ -1,5 +1,8 @@
 package section4_hash_set;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * 4-1 학급회장 (hash)
  *
@@ -11,7 +14,6 @@ package section4_hash_set;
  *
  * [입력]
  * 첫 줄에는 반 학생수 N(5<=N<=50)이 주어집니다.
- *
  * 두 번째 줄에 N개의 투표용지에 쓰여져 있던 각 후보의 기호가 선생님이 발표한 순서대로 문자열로 입력됩니다.
  *
  *
@@ -27,15 +29,56 @@ package section4_hash_set;
  * C
  *
  * [로직]
- *
+ * 1. 5개의 후보를 변수로 정의 (A,B,C,D,E) , 학급회장 변수 (String leader) 초기화, 선출된 투표 수 변수 (int topCount) 초기화
+ * 2. 후보의 기호가 나열된 문자열의 왼쪽부터 오른쪽으로 문자를 탐색 (반복문)
+ * 3. 존재하는 경우 문자열에서 해당 문자를 제거 ('') 하고, 투표수 변수(int count) 값을 증가 시킴
+ * 4. 반복문이 종료되면 선출된 투표수 변수와 비교하여, 큰 경우 topCount 를 count 값으로 업데이트하고, leader 변수의 값도 해당 후보로 업데이트
+ * => 각 후보 별 투표수를 'hash map 의 구조로 생성 및 비교하는것으로 개선해야함'
  *
  * [강의로직]
  *
  *
  * [배운점]
+ * - map 자료구조 : key 와 value 로 이루어진 자료구조
+ * - hash map 이란 :
+ *
  */
 public class No4_1_ClassLeader {
 
+
+    // [강의풀이] 정답
+    /// 다시 풀어볼예정
+    public char solution(int n , String s){
+        char answer = ' ';
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char x : s.toCharArray()){
+            map.put(x, map.getOrDefault(x,0)+1);        //  getOrDefault() : map 에서 key가 존재하면 해당 key의 value 반환, 존재하지 않으면 0 반환
+        }
+        // System.out.println(map.containsKey('A'));               // [참고] A라는 key 가 있는지 확인하는 method (boolean type 반환)
+        // System.out.println(map.size());                         // [참고] map 안의 key 의 갯수 반환
+        // System.out.println(map.remove('A'));                    // [참고] 특정 key 제거
+        int max = Integer.MIN_VALUE;
+        for(char key : map.keySet()){
+            // System.out.println(x + " " +map.get(x));
+            if(map.get(key) > max){
+                max = map.get(key);
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args){
+        No4_1_ClassLeader classLeader = new No4_1_ClassLeader();
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String str = kb.next();
+
+        System.out.println(classLeader.solution(n,str));
+
+    }
 
 
 }
