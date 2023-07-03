9ad8a2a95b6f9dbbeeef6e56c19208f9cdb83a59
DESKTOP-C8583PR\kwang <kwangho410@gmail.com>
2023-07-04 06:57:56 +0900

    [No4_3] 매출액의 종류 문제풀이 (remind)

--- src/section4_hash_set/No4_3_TypeOfSales.java
+++ src/section4_hash_set/No4_3_TypeOfSales.java
@@ -43,28 +43,35 @@ import java.util.Scanner;
  * 예시 출력 1
  * 3 4 4 3
  *
- * [로직] - hash, sliding window 알고리즘
- * 1. 사용자로부터 일자 n과 연속된  K일을  입력받아 int type 변수에 저장
- * 2. n일 동안의 매출기록을 int[] 배열에 저장
- * 3. 포인터 변수 초기화 : start point 0, end point k
- * 4. 매출기록을 반복하면서 hashmap을 요소로 갖는 리스트 (salseSectionList) 에 생성하며 중복된것은 필터링함 (반복문 종료전 포인터들의 값을 1씩 증가함)
- *
+ * [로직]
  *
  */
 public class No4_3_TypeOfSales {
+
+    // 복습예정
     public ArrayList<Integer> solution(int n, int k, int[] arr){
         ArrayList<Integer> answer = new ArrayList<>();
         HashMap<Integer, Integer> HM = new HashMap<>();
+
+        // K일 동안의 매출액 초기값 설정
+        // ex 7일간 매출기록(arr)에서 1~4일까지의 매출액 종류를 HashMap (HM)에 셋팅함
         for(int i=0; i<k-1; i++){
             HM.put(arr[i], HM.getOrDefault(arr[i], 0)+1);
         }
-        int lt=0;
-        for(int rt=k-1; rt<n; rt++){
-            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0)+1);
-            answer.add(HM.size());
+        int lt=0;       // left pointer (two pointer algorithm)
+
+        // sliding window
+        for(int rt=k-1; rt<n; rt++){            // rt 증가 (right pointer)
+
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0)+1); // 매출액 종류를 카운팅하여 HM에 저장)
+            answer.add(HM.size());              // 매출액 종류 저장
+
             HM.put(arr[lt], HM.get(arr[lt])-1);
-            if(HM.get(arr[lt])==0) HM.remove(arr[lt]);
-            lt++;
+
+            if(HM.get(arr[lt])==0) {            // 연속된 K일 동안 매출액(key)이 없는 경우(value = 0인) hashMap 에서 제거
+                HM.remove(arr[lt]);
+            }
+            lt++;       // left pointer 증가
         }
         return answer;
     }
