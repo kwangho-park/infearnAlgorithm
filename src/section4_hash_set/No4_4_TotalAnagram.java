42da48f10a6e790e2e3bbbfb4bfe0026491cd593
DESKTOP-C8583PR\kwang <kwangho410@gmail.com>
2023-07-06 06:51:00 +0900

    [No4_4] 모든 아나그램 문제 풀이중

--- src/section4_hash_set/No4_4_TotalAnagram.java
+++ src/section4_hash_set/No4_4_TotalAnagram.java
@@ -1,7 +1,11 @@
 package section4_hash_set;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
- * 4. 모든 아나그램 찾기
+ * 4. 모든 아나그램 찾기  (hash, sliding window)
  * 설명
  *
  * S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하세요.
@@ -24,8 +28,42 @@ package section4_hash_set;
  * 예시 출력 1
  * 3
  *
+ * [로직]
+ * 1. 문자열 S를  String type 으로 입력받고, 문자열을 요소로가지는 배열(String[]) 로 변환
+ * 2. 문자열 T는 HashMap 에 단어별로 키로 저장
+ * 3. 문자열 배열을 반복하면서 sliging window 구간안에서 hashMap 에 단어와 비교
+ * 4. hashMap에 모든 문자가 존재하는 경우 answer 변수 증가
+ *
  */
 public class No4_4_TotalAnagram {
 
+    public int solution(String s, String t){
+        int answer = 0;
+        HashMap<Character,Integer> HM = new HashMap<>();
 
+        char[] sArr = s.toCharArray();
+
+
+        // HM 초기값 생성
+        for(int loop1=0 ; loop1<t.length() ; loop1++){
+
+        }
+
+        // T와 HM 에 단어를 비교함
+        for(int loop2=0; loop2 <sArr.length; loop2++){
+
+        }
+
+
+        return answer;
+    }
+    public static void main(String[] args){
+        No4_4_TotalAnagram T = new No4_4_TotalAnagram();
+        Scanner kb = new Scanner(System.in);
+
+        String s = kb.nextLine();
+        String t = kb.nextLine();
+
+        System.out.print(T.solution(s,t));
+    }
 }
